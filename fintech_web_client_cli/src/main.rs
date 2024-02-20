b1b5f677810e87c16fec336a8eae84bdbabddd6a
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-02-20 21:37:41 +0100

    Add empty `fintech_web_client_cli`

---
+++ fintech_web_client_cli/src/main.rs
@@ -0,0 +1,8 @@
+//! The "Fintech Web Client CLI" app's entry point.
+
+// use fintech_cli::logic::main_loop;
+
+/// The "Fintech Web Client CLI" app's entry point.
+fn main() {
+    // main_loop();
+}
